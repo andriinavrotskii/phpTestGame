7c3183592da131047cf4f2c18c86647cd61e7007
Andrii Navrotskii <andri.navrotskii@gmail.com>
2017-09-14 01:25:52 +0300

    ready for scaling

--- task.php
+++ task.php
@@ -2,9 +2,35 @@
 
 use Classes\Vehicle;
 
-$vehicle = new Vehicle('bmw');
-$vehicle->fly();
-$vehicle->move();
-$vehicle->musicOn();
-$vehicle->stop();
-$vehicle->refuel('gas');
+$vehicles = [
+    new Vehicle('bmw'), 
+    new Vehicle('boat'), 
+    new Vehicle('helicopter'), 
+    new Vehicle('kamaz')
+];
+
+foreach($vehicles as $vehicle) {
+    switch($vehicle->name)
+    {
+        case 'bmw':
+            $vehicle->move();
+            $vehicle->musicOn();
+            break;
+        case 'boat':
+            $vehicle->move();
+            $vehicle->swim();
+            break;
+        case 'helicopter':
+            $vehicle->takeOff();
+            $vehicle->fly();
+            $vehicle->landing();
+            break;
+        case 'kamaz':
+            $vehicle->move();
+            $vehicle->stop();
+            $vehicle->emptyLoads();
+            break;
+    }
+    $vehicle->stop();
+    $vehicle->refuel('gas');
+}
